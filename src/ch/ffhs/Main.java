51e399f6966b594a901c01a11ed680797d6f2e7b
Melanie Ockenfels <melanie.ockenfels@students.ffhs.ch>
2019-09-09 08:27:48 +0200

    initial layout home

--- src/ch/ffhs/Main.java
+++ src/ch/ffhs/Main.java
@@ -1,23 +1,44 @@
 package ch.ffhs;
 
+import ch.ffhs.Controller.HomeController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    private Stage primaryStage;
 
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        mainView();
+    }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void mainView() {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("Views/home.fxml"));
+            AnchorPane pane = loader.load();
+
+            primaryStage.setMinHeight(600.00);
+            primaryStage.setMinWidth(600.00);
+
+            HomeController homeController = loader.getController();
+            homeController.setMain(this);
+
+            Scene scene = new Scene(pane);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+        }
     }
 }
