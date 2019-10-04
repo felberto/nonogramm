fc75f624a47ba669d90b361e037c800d2da0f0f9
Melanie Ockenfels <melanie.ockenfels@students.ffhs.ch>
2019-10-04 15:02:14 +0200

    load board labels from json

--- src/ch/ffhs/Controller/BoardController.java
+++ src/ch/ffhs/Controller/BoardController.java
@@ -5,8 +5,15 @@ import ch.ffhs.Shared.State;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,7 +34,34 @@ public class BoardController {
             btn_8_0, btn_8_1, btn_8_2, btn_8_3, btn_8_4, btn_8_5, btn_8_6, btn_8_7, btn_8_8, btn_8_9,
             btn_9_0, btn_9_1, btn_9_2, btn_9_3, btn_9_4, btn_9_5, btn_9_6, btn_9_7, btn_9_8, btn_9_9;
 
+    @FXML
+    private Label label_col_0, label_col_1, label_col_2, label_col_3, label_col_4, label_col_5, label_col_6, label_col_7, label_col_8, label_col_9,
+            label_row_0, label_row_1, label_row_2, label_row_3, label_row_4, label_row_5, label_row_6, label_row_7, label_row_8, label_row_9;
+
+    private Label[] columnLabels, rowLabels;
+
     public void initialize() {
+        columnLabels = new Label[]{label_col_0, label_col_1, label_col_2, label_col_3, label_col_4, label_col_5,
+                label_col_6, label_col_7, label_col_8, label_col_9};
+
+        rowLabels = new Label[]{label_row_0, label_row_1, label_row_2, label_row_3, label_row_4, label_row_5,
+                label_row_6, label_row_7, label_row_8, label_row_9};
+
+        // get labels
+        String filePath = "src/ch/ffhs/resources/games.json";
+        try {
+            FileReader reader = new FileReader(filePath);
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+            JSONObject games = (JSONObject) jsonObject.get("games");
+            JSONArray boards = (JSONArray) games.get("10x10");
+            // load first board
+            JSONObject firstBoard = (JSONObject) boards.get(0);
+            setLabels(firstBoard);
+        } catch (IOException | ParseException ex) {
+            System.out.println("Error loading file");
+        }
+
         board = new Board(10);
 
         buttons = new Button[][]{
@@ -44,13 +78,30 @@ public class BoardController {
         };
     }
 
+    private void setLabels(JSONObject board) {
+        JSONArray columns = (JSONArray) board.get("columns");
+
+        for (int i = 0; i < columns.size(); i++) {
+            JSONArray col = (JSONArray) columns.get(i);
+            String colText = col.toString().replace("[", "").replace("]", "").replace(",", "\n");
+            Label colLabel = columnLabels[i];
+            colLabel.setText(colText);
+        }
+
+        JSONArray rows = (JSONArray) board.get("rows");
+        for (int i = 0; i < rows.size(); i++) {
+            JSONArray row = (JSONArray) rows.get(i);
+            String rowText = row.toString().replace("[", "").replace("]", "").replace(",", " ");
+            rowLabels[i].setText(rowText);
+        }
+    }
+
     @FXML
     public void clickButton(final ActionEvent event) {
         Button btn = (Button) event.getSource();
         List<String> index = Arrays.asList(btn.getId().replace("btn_", "").split("_"));
         int row = Integer.parseInt(index.get(0));
         int column = Integer.parseInt(index.get(1));
-        System.out.println(row + " " + column);
         setState(row, column);
     }
 
