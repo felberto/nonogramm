1b69222f53c1810c6574f8e82ed9752e9802190b
felberto <tobias.felber97@gmail.com>
2019-10-22 19:28:36 +0200

    added timer

---
+++ src/main/java/ch/ffhs/Service/CounterService.java
@@ -0,0 +1,37 @@
+package ch.ffhs.Service;
+
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+public class CounterService extends Service<Void> {
+
+    private int timer;
+
+    @Override
+    protected Task<Void> createTask() {
+        return new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                for (int i = timer; true; i++) {
+                    if (isCancelled()) {
+                        break;
+                    }
+
+                    updateProgress(i, Integer.MAX_VALUE);
+                    updateMessage(String.valueOf(i));
+
+                    Thread.sleep(1000);
+                }
+                return null;
+            }
+        };
+    }
+
+    public int getTimer() {
+        return this.timer;
+    }
+
+    public void setTimer(int timer) {
+        this.timer = timer;
+    }
+}
